cf6398710c77801780c559f49111cca48f89459d
stephentetley <stephen.tetley@gmail.com>
2021-04-04 12:19:52 +0100

    Changed System.Directory.contents and contentsAll to use Cursors.

--- src/java/system/FilesPathCursor.java
+++ src/java/system/FilesPathCursor.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package flix.runtime.spt.sandbox.system.marshal;
+package flix.runtime.spt.sandbox.system;
 
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
@@ -22,18 +22,36 @@ import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.stream.Stream;
 
-public class UnmarshalFilesWalk {
+public class FilesPathCursor {
+    
     private Iterator<Path> iter;
 
-    public UnmarshalFilesWalk(Path dir, boolean followLinks) throws Exception {
+    protected FilesPathCursor(Iterator<Path> iter1) {
+        iter = iter1;
+    }
+
+    /// Use a protected constructor and factory methods so we can initialize different
+    /// "processses" with the same cursor / iterator.
+
+    /// Factory method for `Files.walk`
+    public static FilesPathCursor createFilesWalkCursor(Path dir, boolean followLinks) throws Exception {
+        Iterator<Path> iter1;
         if (followLinks) {
             Stream<Path> stream = Files.walk(dir, FileVisitOption.FOLLOW_LINKS);
-            this.iter = stream.iterator();
+            iter1 = stream.iterator();
         } else {
             Stream<Path> stream = Files.walk(dir);
-            this.iter = stream.iterator();
+            iter1 = stream.iterator();
         }
+        return new FilesPathCursor(iter1);
     }
+
+    /// Factory method for `Files.list`
+    public static FilesPathCursor createFilesListCursor(Path dir) throws Exception {
+        Stream<Path> stream = Files.list(dir);
+        return new FilesPathCursor(stream.iterator());
+    }
+
 
     public boolean hasNext() {
         return this.iter.hasNext();
