84823899e8e3af6334d0ddaed0214f1f701c589e
stephentetley <stephen.tetley@gmail.com>
2021-04-04 18:57:16 +0100

    Added a number of Locale functions that need marshalling from Java.

--- src/java/i18n/LocaleStringCursor.java
+++ src/java/i18n/LocaleStringCursor.java
@@ -18,6 +18,7 @@ package flix.runtime.spt.sandbox.i18n;
 
 
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Locale;
 import java.util.Set;
@@ -29,10 +30,31 @@ public class LocaleStringCursor {
         iter = iter1;
     }
 
-    public static LocaleStringCursor createGetISOCountriesCursor(IsoCountryCodeEnum icc) throws Exception {
+    public static LocaleStringCursor createGetISOCountriesCursor() throws Exception {
+        String[] codes = Locale.getISOCountries();
+        return new LocaleStringCursor(Arrays.stream(codes).iterator());
+    }
+
+    public static LocaleStringCursor createGetISOCountriesWithICCCursor(IsoCountryCodeEnum icc) throws Exception {
         Set<String> codes = Locale.getISOCountries(IsoCountryCodeEnum.convertToInner(icc));
         return new LocaleStringCursor(codes.iterator());
     }
+
+    public static LocaleStringCursor createGetISOLanguagesCursor() throws Exception {
+        String[] langs = Locale.getISOLanguages();
+        return new LocaleStringCursor(Arrays.stream(langs).iterator());
+    }
+
+    public static LocaleStringCursor createGetUnicodeLocaleAttributesCursor(Locale locale) throws Exception {
+        Set<String> attrs = locale.getUnicodeLocaleAttributes();
+        return new LocaleStringCursor(attrs.iterator());
+    }
+
+    public static LocaleStringCursor createGetUnicodeLocaleKeysCursor(Locale locale) throws Exception {
+        Set<String> keys = locale.getUnicodeLocaleKeys();
+        return new LocaleStringCursor(keys.iterator());
+    }
+
 
     public boolean hasNext() {
         return this.iter.hasNext();
